1d9a5b11a9593e1064454b55993deacbe8c397a1
zazdr <101953785+zazdr@users.noreply.github.com>
2026-07-25 10:23:16 +0200

    refactor(/app/cmd): use bootstrap

--- app/cmd/exit.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "log"
-
-func exit(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"app/config"
-	"app/db"
-	"app/handler"
-	"app/router"
-	"app/server"
-	"app/sqlc/querier"
-	"app/store"
-	"app/ui"
+	"app/bootstrap"
+	"log"
 )
 
 func main() {
-	config, err := config.New()
-	exit(err)
+	server, err := bootstrap.New()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	db, err := db.New(config)
-	exit(err)
-	querier := querier.New(db)
-	store := store.New(config, querier)
-	ui := ui.New()
-
-	server := server.New(config)
-	handler := handler.New(store, ui)
-	router.New(ui, server, handler)
-
-	exit(server.Start())
+	log.Fatal(server.Start())
 }
